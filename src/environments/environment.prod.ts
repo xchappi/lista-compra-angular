03b7a6501bf75272440d279d6746c6c1378bc76e
Javi Cancela <cancelajavi@gmail.com>
2020-08-11 09:40:26 +0200

    update prod environment & new ci

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyASVKRBl0WgGmw1TWn3QsBT5O7wIcUBbo8',
+    authDomain: 'xchappi-lista-compra.firebaseapp.com',
+    databaseURL: 'https://xchappi-lista-compra.firebaseio.com',
+    projectId: 'xchappi-lista-compra',
+    storageBucket: 'xchappi-lista-compra.appspot.com',
+    messagingSenderId: '431717665970',
+    appId: '1:431717665970:web:75adb4f0e9c08fdc590dab',
+    measurementId: 'G-TDJVGQDX0B',
+  },
 };
